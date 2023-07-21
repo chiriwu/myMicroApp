faef2442f83644ec4c37214ef8c82afcb53f6bbc
chiriwu <18810666617@163.com>
2023-07-21 09:35:13 +0800

    v1

--- examples/vue/src/views/graph/index.vue
+++ examples/vue/src/views/graph/index.vue
@@ -1,9 +1,26 @@
 <template>
   <div class="container">
     <div class="leftElement">
-      <img draggable :src="item" v-for="(item, index) in imgArr" :key="index" @mousedown="move" />
+      <img
+        width="50px"
+        height="50px"
+        draggable
+        :src="item"
+        v-for="(item, index) in imgArr"
+        :key="index"
+        @dragstart="dragStart"
+        @dragend="dragEnd"
+      />
     </div>
     <div id="mountNode"></div>
+    <div class="rightElement">
+      <el-form ref="form" :model="form" label-width="80px">
+        <el-form-item label="活动名称" v-for="key in showKey[curType]" :key="key">
+          <el-input placeholder="请输入内容"></el-input> {{ key }}
+        </el-form-item>
+      </el-form>
+      <el-button @click="test">anjian</el-button>
+    </div>
   </div>
 </template>
 
@@ -12,6 +29,34 @@ import G6 from '@antv/g6';
 export default {
   data() {
     return {
+      startdisX: 0,
+      startdisY: 0,
+      curItem: {},
+      curType: '',
+      form: {},
+      formKey: {
+        id: '',
+        type: '',
+        model: '',
+        group: '',
+        animate: '',
+        visible: '',
+        locked: '',
+        event: '',
+        keyShape: '',
+        states: '',
+        edges: '',
+        styles: '',
+        shapeFactory: '',
+        bboxCache: '',
+        anchorPointsCache: '',
+        originStyle: '',
+        currentShape: '',
+      },
+      showKey: {
+        node: ['currentShape'],
+        edge: ['styles', 'keyShape'],
+      },
       imgArr: [require('@/assets/circle-solid.svg'), require('@/assets/square-solid.svg')],
       number: 1, // 图形自增id
       graph: null,
@@ -57,6 +102,9 @@ export default {
         // 画布宽高
         width: 800,
         height: 500,
+        modes: {
+          default: ['drag-canvas', 'drag-node'],
+        },
       });
     },
     initEvent() {
@@ -76,11 +124,39 @@ export default {
           console.log('type2=', type);
         }
       });
-      this.graph.on('node:click', (ev) => {
-        const node = ev.item; // 被点击的节点元素
-        const shape = ev.target; // 被点击的图形，可根据该信息作出不同响应，以达到局部响应效果
-        console.log('this.is', node._cfg.id);
-        this.addRect();
+      // this.graph.on('node:click', (ev) => {
+      //   const node = ev.item; // 被点击的节点元素
+      //   const shape = ev.target; // 被点击的图形，可根据该信息作出不同响应，以达到局部响应效果
+      //   console.log('this.is', node._cfg.id);
+      //   this.addRect();
+      // });
+      // 点击节点
+      this.graph.on('node:click', (e) => {
+        console.log('hello this is node click');
+        // 先将所有当前是 click 状态的节点置为非 click 状态
+        const clickNodes = this.graph.findAllByState('node', 'click');
+        clickNodes.forEach((cn) => {
+          this.graph.setItemState(cn, 'click', false);
+        });
+        const nodeItem = e.item; // 获取被点击的节点元素对象
+        console.log('nodeItem=', nodeItem);
+        this.curItem = nodeItem;
+        this.curType = 'node';
+        this.graph.setItemState(nodeItem, 'click', true); // 设置当前节点的 click 状态为 true
+      });
+
+      // 点击边
+      this.graph.on('edge:click', (e) => {
+        // 先将所有当前是 click 状态的边置为非 click 状态
+        const clickEdges = this.graph.findAllByState('edge', 'click');
+        clickEdges.forEach((ce) => {
+          this.graph.setItemState(ce, 'click', false);
+        });
+        const edgeItem = e.item; // 获取被点击的边元素对象
+        console.log('edgeItem=', edgeItem);
+        this.curItem = edgeItem;
+        this.curType = 'edge';
+        this.graph.setItemState(edgeItem, 'click', true); // 设置当前边的 click 状态为 true
       });
     },
     addRect(x, y) {
@@ -93,33 +169,30 @@ export default {
         id: `rect-shape${this.number++}`,
       });
     },
-    move(e) {
+    dragStart(e) {
       let odiv = e.target; //获取目标元素
       //算出鼠标相对元素的位置
-      let disX = e.clientX - odiv.offsetLeft;
-      let disY = e.clientY - odiv.offsetTop;
-      console.log('e=', disX, disY);
-      document.onmousemove = (e) => {
-        //鼠标按下并移动的事件
-        //用鼠标的位置减去鼠标相对元素的位置，得到元素的位置
-        let left = e.clientX - disX - 200;
-        let top = e.clientY - disY;
+      this.startDisx = e.clientX - odiv.offsetLeft;
+      this.startDisy = e.clientY - odiv.offsetTop;
+      console.log('disx,disy', this.startDisx, this.startDisy);
+    },
+    dragEnd(e) {
+      let left = e.clientX - this.startDisx - 200;
+      let top = e.clientY - this.startDisy;
+      console.log('good clientD', left, top);
+      this.addRect(left, top);
+    },
+    test() {
+      const model = {
+        id: 'node1',
+        type: 'rect',
+        label: 'node',
+        style: {
+          fill: 'red',
+        },
+      };
 
-        //绑定元素位置到positionX和positionY上面
-        // this.positionX = top;
-        // this.positionY = left;
-        this.addRect(left, top);
-        //移动当前元素
-        //   odiv.style.left = left + 'px';
-        // odiv.style.top = top + 'px';
-        console.log('left=,top=', left, top);
-      };
-      // 没有执行成功这个键盘起来
-      document.onmouseup = (e) => {
-        console.log('this is up=');
-        document.onmousemove = null;
-        document.onmouseup = null;
-      };
+      this.curItem.update(model);
     },
   },
 };
@@ -132,6 +205,13 @@ export default {
   height: 100%;
 }
 .leftElement {
+  width: 200px;
+  border: 1px solid grey;
+}
+#mountNode {
+  flex: 1;
+}
+.rightElement {
   width: 200px;
   border: 1px solid grey;
 }
